f674f5ba0eba6ab7908faee61bce4826c62b5f32
Bohdan <115065189+devBohdan@users.noreply.github.com>
2025-02-27 17:06:56 +0200

    Initial Commit

---
+++ appPodcast/appPodcastApp.swift
@@ -0,0 +1,17 @@
+//
+//  appPodcastApp.swift
+//  appPodcast
+//
+//  Created by Bohdan Kompaniiets on 27.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct appPodcastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
